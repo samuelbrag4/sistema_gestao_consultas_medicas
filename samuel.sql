d39302644f21eebbb923c69f69e23d43485a3ee3
Samuel Braga <samuelbraga.tds2.senai@gmail.com>
2024-11-05 16:29:23 -0300

    fix: Correction of the second query and addition of some data in the tables

--- samuel.sql
+++ samuel.sql
@@ -56,6 +56,11 @@ INSERT INTO pacientes (nome, cpf, data_nascimento, email) VALUES
 ('Samuel', '12345682501', '1995-06-01', 'samuel@exemplo.com'),
 ('Mariana', '83764232109', '1990-01-01', 'mariana@exemplo.com'),
 ('Thiago', '45673412925', '1985-12-31', 'thiago@exemplo.com');
+
+INSERT INTO pacientes (nome, cpf, data_nascimento, email) VALUES 
+('Nathalia', '12345682501', '1995-06-01', 'nathalia@exemplo.com'),
+('Fernanda', '83764232109', '1990-01-01', 'fernanda@exemplo.com'),
+('Bruna', '45673412925', '1985-12-31', 'fernanda@exemplo.com');
 !-- Resultado: INSERT 0 3 --!
 
 !-- Consulta para listar os pacientes --!
@@ -64,9 +69,14 @@ SELECT * FROM pacientes;
 
 !-- Inserção de dados na tabela de médicos --!
 INSERT INTO medicos (nome, cpf, especialidade, email) VALUES 
-('Felipe', '11934678901', 'Clínico Geral', 'felipe@exemplo.com'),
-('Eduardo', '98767242109', 'Pediatra', 'eduardo@exemplo.com'),
-('Marcelo', '45678015305', 'Dermatologista', 'marcelo@exemplo.com');
+('Felipe', '11931745901', 'Clínico Geral', 'felipe@exemplo.com'),
+('Eduardo', '98291264210', 'Pediatra', 'eduardo@exemplo.com'),
+('Marcelo', '45647295305', 'Dermatologista', 'marcelo@exemplo.com');
+
+INSERT INTO medicos (nome, cpf, especialidade, email) VALUES 
+('Agelica', '11918378901', 'Psiquiatra', 'angelica@exemplo.com'),
+('Matlde', '98852364210', 'Ortopedista', 'matilde@exemplo.com'),
+('Jane', '45647295305', 'Cardiologista', 'jane@exemplo.com');
 !-- Resultado: INSERT 0 3 --!
 
 !-- Consulta para listar os médicos --!
@@ -78,6 +88,11 @@ INSERT INTO consultas (id_paciente, id_medico, data_consulta, horario, valor) VA
 (2, 2, '2022-10-15', '09:30', 200.00),
 (3, 1, '2021-07-23', '10:00', 250.00),
 (1, 3, '2024-01-27', '10:30', 400.00);
+
+INSERT INTO consultas (id_paciente, id_medico, data_consulta, horario, valor) VALUES 
+(4, 6, '2024-11-15', '09:30', 300.00),
+(6, 4, '2024-12-31', '10:00', 150.00),
+(5, 5, '2024-12-25', '10:30', 500.00);
 !-- Resultado: INSERT 0 3 --!
 
 !-- Consulta para listar as consultas --!
@@ -92,12 +107,12 @@ JOIN medicos m ON c.id_medico = m.id_medico
 WHERE c.data_consulta <= CURRENT_DATE;
 !-- Resultado: nome_paciente | nome_medico | data_consulta | especialidade --!
 
-!-- Consulta para listar TODOS os pacientes (até mesmo os com consultas futuras) --!
-SELECT p.nome AS nome_pacientes, m.nome AS nome_medico, c.data_consulta, m.especialidade
-FROM consultas c
-JOIN pacientes p ON c.id_paciente = p.id_paciente
-JOIN medicos m ON c.id_medico = m.id_medico;
-!-- Resultado: nome_paciente | nome_medico | data_consulta | especialidade --!
+!-- Consulta para mostrar todos os pacientes que não realizaram nenhuma consulta (passadas ou futuro). --!
+SELECT p.nome AS paciente, p.cpf, p.data_nascimento, p.email
+FROM pacientes p
+LEFT JOIN consultas c ON p.id_paciente = c.id_paciente
+WHERE c.id_paciente IS NULL;
+!-- Resultado: nome_paciente | cpf | data_nascimento | email --!
 
 !-- Consulta para listar os médicos que ainda não realizaram nenhuma consulta --!
 SELECT m.nome AS nome_medico, m.especialidade
